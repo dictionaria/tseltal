2f0e0738a3c75c5a57831cec51df5b98f957181d
Johannes Englisch <englisch@shh.mpg.de>
2021-02-08 13:50:53 +0100

    cldfbench

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+setup(
+    name='cldfbench_tseltal',
+    py_modules=['cldfbench_tseltal'],
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        'cldfbench.dataset': [
+            'tseltal=cldfbench_tseltal:Dataset',
+        ]
+    },
+    install_requires=[
+        'cldfbench',
+        'pydictionaria',
+    ],
+    extras_require={
+        'test': [
+            'pytest-cldf',
+        ],
+    },
+)
